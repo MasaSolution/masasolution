ed5713a9baeb0ddf86959f0ddff4834239b62e1a
Norbert Toth <norberttoth@green.sophos>
2018-04-25 15:30:44 +0200

    Add service images

---
+++ home.jsx
@@ -0,0 +1,17 @@
+import React from "react"
+import ReactDOM from "react-dom"
+
+export default class Home extends React.Component {
+    constructor() {
+        super();
+    }
+
+    render() {
+        return (
+            <div>
+                <h1>Welcome on MasaSolution</h1>
+                <img src="construction.jpg" alt="Page is under construction"/>
+            </div>
+        );
+    }
+}
