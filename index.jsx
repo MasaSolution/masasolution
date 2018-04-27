99edeae74aae47e6cee02f4ef905825fe4b4f94b
Norbert Toth <norberttoth@green.sophos>
2018-04-27 16:01:36 +0200

    Deliver commit

--- index.jsx
+++ index.jsx
@@ -5,7 +5,6 @@ import Home from "./home.jsx"
 import Services from "./services.jsx"
 import Products from "./products.jsx"
 import Contact from "./contact.jsx"
-import { Z_FIXED } from "zlib";
 
 export default class Index extends React.Component {
     constructor() {
@@ -14,19 +13,21 @@ export default class Index extends React.Component {
 
     render() {
         return (
-            <div className="container">
-                <div className="sidePane">
-                    <nav className="nav flex-column">
-                        <a className="border rounded nav-link" href="#home">Home</a>
-                        <a className="border rounded nav-link" href="#services">Services</a>
-                        <a className="border rounded nav-link" href="#products">Products</a>
-                    </nav>
-                    <Contact key="contact" id="contact"/>
-                </div>
-                <div className="contentPane">
-                    <Home key="home" id="home" />
-                    <Services key="services" id="services" />
-                    <Products key="products" id="products" />
+            <div className="container-fluid">
+                <div className="row">
+                    <div className="sidePane col-2">
+                        <nav className="nav flex-column">
+                            <a className="border rounded nav-link" href="#home">Home</a>
+                            <a className="border rounded nav-link" href="#services">Services</a>
+                            <a className="border rounded nav-link" href="#products">Products</a>
+                        </nav>
+                        <Contact key="contact" id="contact" />
+                    </div>
+                    <div className="contentPane col-6">
+                        <Home key="home" id="home" />
+                        <Services key="services" id="services" />
+                        <Products key="products" id="products" />
+                    </div>
                 </div>
             </div>
         );
