34f49a229d8d7b6c2eebe35406e7e10cb3157f9c
moravas <norbert.toth.ees@gmail.com>
2018-05-13 14:38:59 +0200

    Update build

--- contact.jsx
+++ contact.jsx
@@ -12,9 +12,7 @@ export default class Contact extends React.Component {
                 <div className="card-header">MasaSolution</div>
                 <div className="card-body">
                     <h5 className="card-title">Sales & Contact: <a href="mailto:contact.masasolution@gmail.com?subject=Contact">contact.masasolution@gmail.com</a></h5>
-                    <p className="card-text">If you like our products</p>
-                    <p className="card-text">If you want a solution that nobody has</p>
-                    <p className="card-text">Drop an email and we will contact to you</p>
+                    <p className="card-text">If you like our products<br/>If you want a solution that nobody has<br/>Drop an email and we will contact to you</p>
                 </div>
             </div>
         );
