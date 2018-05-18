51554da348362f0feb74c5e049e72c0805500980
moraavs <moravas8051@gmail.com>
2018-05-18 09:00:05 +0200

    Reorganize file structure

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ var path = require('path');
 var webpack = require('webpack');
 
 module.exports = {
-  entry: './index.jsx',
+  entry: './src/index.jsx',
   output: {
     path: __dirname,
     filename: 'index.bundle.js'
